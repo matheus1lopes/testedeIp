1c532153660087dcb6919d8f66f7da33e00a51d5
matheus1lopes <161980738+matheus1lopes@users.noreply.github.com>
2024-05-03 23:28:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script language="javascript">
+function LocalizaIP_done(ip_data) {
+if (!ip_data['error'] //your code goes here
+alert('Localizei IP: '+ip_data['city']+'-'+ip_data['country']+' (lat: '+ip_data['latitude']+' , lon: '+ip_data['longitude']+')');
+}
+</script>
